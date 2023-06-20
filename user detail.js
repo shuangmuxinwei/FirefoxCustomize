98495eefc2370010b1692efce9d7a350f7170353
shuangmuxinwei <137176115+shuangmuxinwei@users.noreply.github.com>
2023-06-20 21:45:42 +0800

    Create user detail.js

---
+++ user detail.js
@@ -0,0 +1,24 @@
+/*==========常规>启动==========*/
+// 取消默认浏览器检查Do not check if Firefox is the default browser
+user_pref("browser.shell.checkDefaultBrowser", false);
+// 设置启动的开始页面。3=打开先前的窗口和标签页。set startup page. 0=blank, 1=home, 2=last visited page, 3=resume previous session.
+// [SETTING] General>Startup>Restore previous session 
+user_pref("browser.startup.page", 1);
+// 禁止火狐升级后跳转至介绍新版本特性的页面disable welcome notices. After a browser update, Firefox might show an additional tab next to your usual homepage to offer more information on changes or new features included in the update. The browser’s milestone (e.g., "rv:1.8.1.1") from the last successful startup of the browser. If it differs from the browser’s actual milestone, it is assumed an upgrade has occurred, and the user is redirected to the homepage override URL. If its value is "ignore", then the redirect does not occur and the preference is not updated. 
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+// 禁止火狐升级后跳转至介绍新版本特性的页面When a user starts up their browser after upgrading (i.e., if browser.startup.homepage_override.mstone is different than the last time the browser started), the URL specified in this preference is loaded instead of their homepage.
+user_pref("startup.homepage_override_url", "");
+// 禁止火狐首次启动时跳转页面When Firefox is started with a new profile (usually, the first time it’s started after installation), the URL specified in this preference will be loaded (even if the user imported their own homepage). This is similar to startup.homepage_override_url, which specifies the URL loaded when Firefox is first started after an upgrade, but this preference allows specifying the page a first-time user would see.
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+// 禁止启动时显示空白页，加快启动速度disable showing about:blank/maximized window as soon as possible during startup [FF60+]
+user_pref("browser.startup.blankWindow", false);
+// 在加载libxul之前，显示skeleton UI窗口Show a skeleton UI window prior to loading libxul. Only visible for windows users as it is not implemented anywhere else.
+user_pref("browser.startup.preXulSkeletonUI", false);
+/*=====主页>新窗口和标签页=====*/
+// 设置主页和新窗口的页面set HOME+NEWWINDOW page. about:home=Firefox Home , custom URL, about:blank
+// [SETTING] Home>New Windows and Tabs>Homepage and new windows
+user_pref("browser.startup.homepage", "chrome://browser/content/places/places.xhtml");
+// 设置新标签页页面set NEWTAB page. true=Firefox Home (default), false=blank page
+// [SETTING] Home>New Windows and Tabs>New tabs
+user_pref("browser.newtabpage.enabled", false);
